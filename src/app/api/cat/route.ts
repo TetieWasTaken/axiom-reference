80203c86b77464e6f6ccc72572612a35a4216714
TetieWasTaken <tjvssr@gmail.com>
2025-06-22 20:08:16 +0200

    feat: cat api

---
+++ src/app/api/cat/route.ts
@@ -0,0 +1,21 @@
+import type { NextRequest } from "next/server";
+import { createClient } from "@/utils/supabase/server";
+
+export async function GET(request: NextRequest) {
+	const supabase = await createClient();
+	const searchParams = request.nextUrl.searchParams;
+	const subject = searchParams.get("subject");
+
+	const { data, error } = await supabase
+		.from("constructs")
+		.select("slug, name, subject, subsubject")
+		.eq("subject", subject);
+
+	if (error) {
+		return new Response(error.message, {
+			status: 500,
+		});
+	}
+
+	return Response.json(data);
+}
