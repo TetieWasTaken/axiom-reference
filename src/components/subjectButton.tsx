735f65e0061bed1e88bd1b07b00a1e6ef963df2b
TetieWasTaken <tjvssr@gmail.com>
2025-06-22 20:41:51 +0200

    feat: route to cat page

---
+++ src/components/subjectButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+
+interface SubjectButtonProps {
+	subject: string;
+}
+
+export default function SubjectButton({ subject }: SubjectButtonProps) {
+	const router = useRouter();
+
+	return (
+		<button
+			onClick={() => router.push(`/cat/${subject}`)}
+			className={`relative inline-block mx-6 px-6 py-3 text-lg font-semibold text-white group`}
+		>
+			<span className="absolute inset-0 w-full h-full transition-transform transform scale-100 bg-gradient-to-r from-red-500 via-rose-500 to-indigo-600 rounded-lg blur-sm opacity-50 group-hover:opacity-80 group-hover:scale-105"></span>
+			<span className="relative z-10 block px-6 py-3 bg-slate-900 rounded-lg border border-slate-700 group-hover:border-red-500 transition-colors duration-300 font-title">
+				Go to {subject}
+			</span>
+		</button>
+	);
+}
